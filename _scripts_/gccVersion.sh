3620f27016c99e504af41564912727ef1b5a3d5f
Thomas Weise <tweise@hfuu.edu.cn>
2026-05-09 18:31:17 +0800

    trying to find error 5

--- _scripts_/gccVersion.sh
+++ _scripts_/gccVersion.sh
@@ -8,8 +8,11 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   # set -u : exit the script if you try to use an uninitialized variable
 set -o errexit   # set -e : exit the script if any statement returns a non-true return value
 
+echo "test"
 scriptDir="$(dirname "${BASH_SOURCE[0]}")"
+echo "$scriptDir"
 scriptDir="$(realpath -eP "$scriptDir")"
+echo "$scriptDir"
 gccStd="$($scriptDir/gccStd.sh)"
 echo "$gccStd"
 gccVersion="$(gcc --std=$gccStd --version)"
