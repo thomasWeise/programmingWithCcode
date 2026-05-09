8ae204afa1aaf4fd2ef6d2873323f7fe10647c82
Thomas Weise <tweise@hfuu.edu.cn>
2026-05-09 17:14:32 +0800

    attempt to see what's wrong

--- _scripts_/gccVersion.sh
+++ _scripts_/gccVersion.sh
@@ -8,7 +8,8 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   # set -u : exit the script if you try to use an uninitialized variable
 set -o errexit   # set -e : exit the script if any statement returns a non-true return value
 
-scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+scriptDir="$(dirname "${BASH_SOURCE[0]}")"
+scriptDir="$(realpath -eP "$scriptDir")"
 gccStd="$($scriptDir/gccStd.sh)"
 gccVersion="$(gcc --std=$gccStd --version)"
 gccVersion="$(grep "^gcc" <<< "$gccVersion")"
